92dab3d7ff47409c8112fc9eb1442f306da91593
EfraPayon777 <efraincortez777@gmail.com>
2026-07-13 08:19:16 -0600

    LAB 3 Migraciones

--- MVCPeliculas/Data/PeliculasDbContext.cs
+++ MVCPeliculas/Data/PeliculasDbContext.cs
@@ -2,10 +2,14 @@
 using MVCPeliculas.Models;
 namespace MVCPeliculas.Data;
 
+
 public class PeliculasDbContext : DbContext
 {
     public PeliculasDbContext(DbContextOptions options) : base(options)
     {
+
     }
+
     public DbSet<Pelicula> Peliculas { get; set; }
+    public DbSet<Genero> Generos { get; set; } // <- Nuevo modelo 
 }
